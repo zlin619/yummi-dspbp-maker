d20cb63e00043c796602e76457dc18b33a1c2a85
zlin619 <145180341+zlin619@users.noreply.github.com>
2025-05-04 22:07:25 +0800

    实现传送带和分拣器的参数序列化和反序列化 (#4)
    实现传送带和分拣器的参数序列化和反序列化
    并为其他设施留了个空
    这里写的有点蠢, 尤其是补0写的, 以后看看能不能改
    后面的设备,除了制造台外,我并不打算实现有损压缩
    
    modified: 文件/签名.py
    modified: 文件/蓝图文件解析/解析中段.py
    modified: 蓝图库/临时/输入所有建筑.json
    modified: 蓝图库/临时/输入蓝图.txt
    modified: 蓝图格式/额外参数.py

--- 蓝图格式/额外参数.py
+++ 蓝图格式/额外参数.py
@@ -14,11 +14,13 @@ from 蓝图格式.模型 import 模型
 class 额外参数(蓝图dataclass基类):
     def 转比特流(self) -> bytes:
         raise NotImplementedError(f"谢谢你，这个算虚基类方法，请重写方法。")
-    
+
     @classmethod
     def 由json转换(cls, 数据字典):
         if 数据字典["参数类型"] == "未解析":
             return 额外参数之未解析.由json转换(数据字典)
+        if 数据字典["参数类型"] == "空白":
+            return 额外参数之空白.由json转换(数据字典)
         elif 数据字典["参数类型"] == "传送带":
             return 额外参数之传送带.由json转换(数据字典)
         elif 数据字典["参数类型"] == "分拣器":
@@ -35,6 +37,7 @@ class 额外参数(蓝图dataclass基类):
             return 额外参数之制造类建筑.由json转换(数据字典)
         else:
             raise NotImplementedError("伪纯虚基类，还想执行方法，想屁吃？")
+
 
 @dataclass
 class 额外参数之未解析(额外参数):
@@ -59,6 +62,27 @@ class 额外参数之未解析(额外参数):
     def 由json转换(cls, 数据字典):
         return cls(**数据字典)
 
+    def 尝试解析(self, 模型) -> 额外参数:
+        if len(self.参数) == 0:
+            return 额外参数之空白()
+        elif 模型 not in 额外参数对应关系:
+            return self
+        else:
+            return 额外参数对应关系[模型].尝试构造(self)
+
+@dataclass
+class 额外参数之空白(额外参数):
+    参数类型: str = "空白"
+
+    @classmethod
+    def 由json转换(cls, 数据字典):
+        return cls(**数据字典)
+
+    def 转比特流(self) -> bytes:
+        流数据 = bytearray()
+        流数据.extend(struct.pack("<H", 0))
+        return 流数据
+
 @dataclass
 class 额外参数之传送带(额外参数):
     图标ID: 图标
@@ -69,6 +93,35 @@ class 额外参数之传送带(额外参数):
     def 由json转换(cls, 数据字典):
         return cls(**数据字典)
 
+    @classmethod
+    def 尝试构造(cls, 未解析参数: 额外参数之未解析) -> 额外参数:
+        # 按理说参数长度只应该为0或2,除此以外都是未定义行为
+        # 未定义行为, 但是有效
+        l_图标ID = 图标(未解析参数.参数[0])
+        if len(未解析参数.参数) >= 2:
+            l_图标数字 = 类型.Int32(未解析参数.参数[1])
+        else:
+            l_图标数字 = 类型.Int32(0)
+        return 额外参数之传送带(图标ID = l_图标ID, 图标数字 = l_图标数字)
+
+
+    def 转比特流(self) -> bytes:
+        流数据 = bytearray()
+        流数据.extend(struct.pack
+            ("<Hii",
+            2,
+            self.图标ID.转int(),
+            self.图标数字
+        ))
+        return 流数据
+
+    # 充分利用未定义行为,未经过测试,就先扔这里再说
+    def 有损压缩转解析前(self) -> bytes:
+        if self.图标ID.转int() == 0:
+            return 额外参数之空白()
+        elif self.图标数字 == 0:
+            return 额外参数之未解析([类型.Int32(self.图标ID.转int())])
+
 @dataclass
 class 额外参数之分拣器(额外参数):
     分拣器长度: 类型.Int32 # 分拣器长度 -> 1-3
@@ -77,6 +130,25 @@ class 额外参数之分拣器(额外参数):
     @classmethod
     def 由json转换(cls, 数据字典):
         return cls(**数据字典)
+
+    @classmethod
+    def 尝试构造(cls, 未解析参数: 额外参数之未解析) -> 额外参数:
+        # 按理说参数长度只应该为0或2,除此以外都是未定义行为
+        # 未定义行为, 但是有效
+        return 额外参数之分拣器(分拣器长度 = 类型.Int32(未解析参数.参数[0]))
+
+    def 转比特流(self) -> bytes:
+        流数据 = bytearray()
+        流数据.extend(struct.pack
+            ("<Hi",
+            1,
+            self.分拣器长度,
+        ))
+        return 流数据
+
+    # 充分利用未定义行为,未经过测试,就先扔这里再说
+    def 有损压缩转解析前(self) -> bytes:
+        return 额外参数之空白()
 
 @dataclass
 class 额外参数之储液罐(额外参数):
@@ -132,3 +204,13 @@ class 额外参数之制造类建筑(额外参数):
 # 炮台类建筑
 # 四向
 # 箱子
+
+额外参数对应关系 = {
+    模型.名字转序号("传送带"): 额外参数之传送带,
+    模型.名字转序号("高速传送带"): 额外参数之传送带,
+    模型.名字转序号("极速传送带"): 额外参数之传送带,
+    模型.名字转序号("分拣器"): 额外参数之分拣器,
+    模型.名字转序号("高速分拣器"): 额外参数之分拣器,
+    模型.名字转序号("极速分拣器"): 额外参数之分拣器,
+    模型.名字转序号("集装分拣器"): 额外参数之分拣器,
+}
