2023eef856137d90cf28bd83fb107b1fea24832d
Zlin <11281308+huang-zhilin2@user.noreply.gitee.com>
2025-05-11 22:40:20 +0800

    quick look

--- 日志.py
+++ 日志.py
@@ -19,7 +19,13 @@ def 信息(警告信息):
 def 未完成的函数(str):
     raise Exception("函数还没写完")
 
+def 纯虚函数():
+    raise Exception(f"尝试执行纯虚函数")
+
+def 确保类型(输入, 类型: type):
+    if isinstance(输入, type):
+        raise Exception(f"输入类型错误:{输入.__class__}")
+
 def 日志(str):
     print(str)
-
 
